8e2a977ab4e178e7e2d6deb6f438ef291d82b1d6
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-11 11:07:04 +0200

    added movment in right site when user presing 'a'

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ player = Entity(model ='sphere', color = rgb(230, 143, 67), scale = (3,3,1) )
 
 def update():
     player.x += held_keys['d'] * time.dt
+    player.x -= held_keys['a'] * time.dt
 
 app.run()
