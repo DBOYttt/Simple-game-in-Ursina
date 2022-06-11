cc0c663ba8474506d94307e674029888280588f4
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-06-11 11:25:28 +0200

    add jump

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ app = Ursina()
 
 player = Entity(model ='sphere', color = rgb(230, 143, 67), scale = (3,3,1) )
 
+
 def update():
     player.x += held_keys['d'] * time.dt
     player.x -= held_keys['a'] * time.dt
 
-app.run()
+def input(key):
+    if key == 'space':
+        player.y += 1
+        invoke(setattr, player, 'y', player.y - 1, delay = .25)
+
+if player.position > 1:
+    player.position = Vec3(0,0,0)
+
+app.run() 
